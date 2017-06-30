6e3cae56d196c3c7a23667a8b81fec5f7bae777c
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-06-30 01:18:36 -0700

    most recent tweet is tracked, code is refactored

--- database.sql
+++ database.sql
@@ -5,7 +5,15 @@ CREATE OR REPLACE DATABASE tweetThreat;
 USE tweetThreat;
 
 CREATE TABLE tweets(
-    tweet_id BIGINT NOT NULL PRIMARY KEY,
+    tweet_id BIGINT UNSIGNED NOT NULL PRIMARY KEY,
     date_time DATETIME DEFAULT NULL,
     user_id BIGINT DEFAULT NULL
 );
+
+CREATE TABLE users(
+    user_id BIGINT UNSIGNED NOT NULL PRIMARY KEY,
+    screen_name VARCHAR(30) NOT NULL,
+    most_recent_tracked_tweet BIGINT UNSIGNED NOT NULL
+);
+
+INSERT INTO users (user_id, screen_name, most_recent_tracked_tweet) VALUES(25073877, 'realDonaldTrump', 822421390125043713);
--- worker.js
+++ worker.js
@@ -5,19 +5,41 @@ var m = Maria({
         db: 'tweetThreat'
 });
 
-//var tracker = new Array(24).fill(0); //will track number of tweets posted during each hour in day
 
-var error = function (err, response, body) {
-    	console.log('ERROR:', err);
-	};
-var success = function (data) {
-    addTimes( JSON.parse(data) );
+var Twitter = require('twitter-node-client').Twitter;
+
+var config = {
+    "consumerKey": "gOpaxDx9KB7EHhucQbpICHTYK",
+	"consumerSecret": "QuDGs1ogoEAF2O4T9Wf4vtMOVQuMo4ZFvy8ub4Wh6F38qgRIAG",
+	"accessToken": "2577156107-Cis4tS6o9IyN5I9VHjzNs5PPrEtv7Fy2tyV6xPx",
+	"accessTokenSecret": "OMiOdz5Vzl0py33y9inu6cwTxVMoL1o4AagqFN4pUwJLd"
 };
 
-var addTimes = function (tweets) {
+var twitter = new Twitter(config);
+
+var error = function (err) {
+	if (err){
+    	console.log('ERROR:', err);
+	}
+};
+
+var TweetGetter = function(sinceId) {
+	this.sinceId = sinceId;
+	this.maxId = null;
+	this.latest = null;
+}
+
+TweetGetter.prototype.addTimes = function (data) {
+	var tweets = JSON.parse(data);
+
 	if (tweets.length === 0 ){
 		m.end();
 		return;
+	}
+
+	if (this.latest === null){
+		this.latest = tweets[0].id;
+		m.query('UPDATE users SET most_recent_tracked_tweet = :latest where screen_name = "realDonaldTrump"', {latest: this.latest}, error);
 	}
 
 	//for each tweet in the array, insert tweet.id and tweet.created_at into the tweets table
@@ -25,40 +47,36 @@ var addTimes = function (tweets) {
 		let id = tweet.id;
 		let date = new Date(tweet.created_at);
 		date = date.toISOString();
-		m.query('INSERT IGNORE INTO tweets (tweet_id, date_time) VALUES (:id, :date)', {id, date}, function (error, sucess){
-			if (error){
-				console.log(error, date);
-			}
-		});
+		m.query('INSERT IGNORE INTO tweets (tweet_id, date_time) VALUES (:id, :date)', {id, date}, error);
 	});
 
 	//get the next page of tweets after this one
-	let lastId = tweets[tweets.length - 1].id;
-	getTimes(lastId);
-}
+	this.maxId = tweets[tweets.length - 1].id;
+	this.getTimes();
+};
 
-var Twitter = require('twitter-node-client').Twitter;
-
-var config = {
-    	"consumerKey": "gOpaxDx9KB7EHhucQbpICHTYK",
-    	"consumerSecret": "QuDGs1ogoEAF2O4T9Wf4vtMOVQuMo4ZFvy8ub4Wh6F38qgRIAG",
-    	"accessToken": "2577156107-Cis4tS6o9IyN5I9VHjzNs5PPrEtv7Fy2tyV6xPx",
-    	"accessTokenSecret": "OMiOdz5Vzl0py33y9inu6cwTxVMoL1o4AagqFN4pUwJLd",
-    }
-
-var twitter = new Twitter(config);
-
-var getTimes = function (maxId) {
-	let options = { screen_name: 'realDonaldTrump',
-					count: '200',
-					trim_user: true,
-					since_id: 822421390125043713}
+TweetGetter.prototype.getTimes = function () {
+	let options = { 
+		screen_name: 'realDonaldTrump',
+		count: '200',
+		trim_user: true
+	};
 	
-	if (maxId !== undefined) {
-		options.max_id = maxId;
+	if (this.maxId){
+		options.max_id = this.maxId;
 	}
-	
-	twitter.getUserTimeline(options, error, success);
-}
 
-getTimes();
+	options.since_id = this.sinceId;
+	
+	twitter.getUserTimeline(options, error, this.addTimes.bind(this));
+};
+
+m.query('SELECT most_recent_tracked_tweet from users where screen_name = "realDonaldTrump"', null, function(error, success){
+	if (error){
+		console.log(error);
+	} else {
+		let sinceId = success[0].most_recent_tracked_tweet;
+		let worker = new TweetGetter(sinceId);
+		worker.getTimes();
+	}
+});
