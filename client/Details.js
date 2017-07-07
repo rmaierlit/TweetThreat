e1bb13186d3775d3a16df136190ea1ead50e55f6
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-07-07 14:39:07 -0700

    creates seperate style sheets for portrait and landscape views to improve responsiveness on mobile screens

--- client/Details.js
+++ client/Details.js
@@ -1,16 +1,13 @@
 import React from 'react';
 
 function Details(props) {
-    if (props.latest === null){
-        return null;
-    }
     return (
         <div className='details'>
             <p>Name: <b>Donald J. Trump</b></p>
             <p>Alias: <b>@realDonaldTrump</b></p>
             <p>Followers: <b>33.4M</b></p>
             <p><b>{props.risk}%</b> risk of tweeting in next hour (as of {props.time})}</p>
-            <p>Last Known Tweet: <b>{props.latest.created_at}</b></p>
+            <p>Last Known Tweet: <b>{props.latest? props.latest.created_at: 'Analysing...'}</b></p>
         </div>
     );
 }
