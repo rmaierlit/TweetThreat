6b2c9e16d6f1877b6dad7ca3d34582823c51f71a
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-07-07 18:38:19 -0700

    replaces tweet module with one that uses the offical twitter embedding framework

--- client/LatestTweet.js
+++ client/LatestTweet.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Tweet from 'react-tweet';
+import Tweet from 'react-tweet-embed';
 
 function LatestTweet(props) {
     if (props.data === null) {
         return null;
     } else {
         return (
-            <Tweet data={props.data} />
+            <Tweet id={props.data.id_str} options={{cards:'hidden', conversation:'none'}} />
         );
     }
 }
