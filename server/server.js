64768a06ea8460d97e50646d588ac2340ad0727f
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-07-08 22:07:59 -0700

    changed port to 80 for demo (unblocked on digitalOcean)

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var app = express();
 //app.use('/api', router);
 app.use(express.static(__dirname + '/../client'));
 
-app.listen(2525);
+app.listen(80);
 
 
-console.log('server listening on 2525');
+console.log('server listening on 80');
