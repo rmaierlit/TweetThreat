7ee716360a7c2df4b17aaad36099a1d8226ee834
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-07-09 22:51:12 -0700

    reduces font size for details to avoid formatting problems on smartphones

--- client/app.js
+++ client/app.js
@@ -1,27 +1,38 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import axios from 'axios';
+import LatestTweet from './LatestTweet.js';
 import LevelList from './LevelList.js';
 import Details from './Details.js';
+
+const tweetsByHour = [97, 84, 99, 68, 39, 12, 10, 3, 0, 10, 54, 107, 151, 143, 99, 78, 80, 96, 87, 92, 110, 138, 121, 109]
+const totalDays = 410;
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {user: 'realDonaldTrump', risk: 30, time: 'lol', latest: null};
+        this.state = {user: 'realDonaldTrump', risk: null, time: null, latest: null};
     }
 
-    /*getRisk() {
-        axios.get(`http://localhost:2525/api/${this.state.user}/risk`)
-            .then( res => this.setState(res.data));
-    }*/
+    getRisk() {
+        let now = new Date();
+        let hour = parseInt(now.getUTCHours());
+        let minute = parseInt(now.getUTCMinutes());
+        let currentHourRisk = tweetsByHour[hour]/ totalDays;
+        let nextHourRisk = tweetsByHour[ (hour + 1) % 24] / totalDays;
+        let estimatedRisk = (currentHourRisk * (60 - minute) / 60) + (nextHourRisk * minute / 60);
+        let percentRisk = Math.round(estimatedRisk * 100);
+
+        this.setState({risk: percentRisk, time: now.toLocaleTimeString()});
+    }
 
     getLatestTweet() {
-        axios.get(`http://localhost:80/api/${this.state.user}/latestTweet`)
+        axios.get(`api/${this.state.user}/latestTweet`)
             .then( res => this.setState({latest: res.data}) );
     }
 
     componentWillMount() {
-        //this.getRisk();
+        this.getRisk();
         this.getLatestTweet();
     }
 
@@ -37,6 +48,7 @@ class App extends Component {
                                  time={this.state.time}
                                  latest={this.state.latest} />
                         </div>
+                        <LatestTweet data={this.state.latest} />
                     </div>
                     <LevelList user={this.state.user} risk={this.state.risk}/>
                 </div>
